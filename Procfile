9502ae282503ec81c38da244c08004f3f1914e9b
Nick A <nicolas.a.asanov@gmail.com>
2025-01-15 00:29:17 -0500

    dep

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: ls client/build && tsc && node dist/index.js
+web: tsc && node dist/index.js
--- index.ts
+++ index.ts
@@ -16,13 +16,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "..", "client", "build")))
-
 app.use(cors({
     credentials: true,
     origin: process.env.FRONTEND_URL || "http://localhost:3000",
     optionsSuccessStatus: 200
 }))
+app.use(express.static(path.join(__dirname, "..", "client", "build")))
 
 app.use('/api/auth', auth);
 app.use('/api/users', users);
